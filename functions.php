9060c361019e7edbadfbf9f4a1cd4b8885093ef0
akky <akimoto+github@gmail.com>
2018-11-20 23:12:08 +0900

    fixed warnings on PHP7.
      * suppress warning where non-existing array key was accessed.
      * use __construct() instead of the class name function.

--- functions.php
+++ functions.php
@@ -6,69 +6,73 @@ add_filter('pre_get_posts', 'my_get_posts');
 // local tag list
 
 function tag_list_widget_setup() {
-	register_widget('Tag_List_Widget');
+    register_widget('Tag_List_Widget');
 }
 
 function my_get_posts( $query ) 
 {
-	if ( (is_home() || is_search() || is_tag() || is_category()) && false == $query->query_vars['suppress_filters'] )
-		$query->set( 'post_type', array( 'post', 'bookmark' ) );
+    $isSuppressFilters = false;
+    if (array_key_exists('suppress_filters', $query->query_vars)) {
+        $isSuppressFilters = ($query->query_vars['suppress_filters'] != false);
+    }
+    if ( (is_home() || is_search() || is_tag() || is_category()) && !$isSuppressFilters )
+        $query->set( 'post_type', array( 'post', 'bookmark' ) );
 
-	return $query;
+    return $query;
 }
 
 class Tag_List_Widget extends WP_Widget {
 
-	function Tag_List_Widget() {
-			$widget_ops = array( 'description' => __('Lists all tags') );
-			$control_ops = array( 'width' => 400, 'height' => 200 );
-			$this->WP_Widget( 'tag_list', __('Tag List'), $widget_ops, $control_ops );
-		}
+    function __construct() {
+            $widget_ops = array( 'description' => __('Lists all tags') );
+            $control_ops = array( 'width' => 400, 'height' => 200 );
+            parent::__construct( 'tag_list', __('Tag List'), $widget_ops, $control_ops );
+        }
 
-	function widget($args, $instance) {
-			extract( $args );
-			echo $before_widget; 
-			
-			$title = apply_filters('widget_title', empty( $instance['title'] ) ? __( 'Top Tags' ) : $instance['title']);
-			echo $before_title . $title . $after_title;
-			?>
-		    <ul id="tag-list-widget">
-			<?php
-			$tags = get_terms('post_tag', array(
-				'fields' => 'all', 
-			    'orderby' => 'count',
-				'order' => 'DESC',
-				'number' => $instance['num']  ));
-			foreach ($tags as $tag) {
-				echo '<li><a href="/tag/'.$tag->slug.'">'.$tag->name.'<span class="count">'.$tag->count.'</span></a></li>';
-			}
-			
-			?>
-		    </ul>
-		   <?php
-			echo $after_widget;
-	}
-	
-	function update( $new_instance, $old_instance ) {
-			$instance = $old_instance;
-			$instance['title'] = strip_tags($new_instance['title']);
-			$instance['num'] = (int)$new_instance['num'];
-			return $instance;
-	}
+    function widget($args, $instance) {
+            extract( $args );
+            echo $before_widget; 
+            
+            $title = apply_filters('widget_title', empty( $instance['title'] ) ? __( 'Top Tags' ) : $instance['title']);
+            echo $before_title . $title . $after_title;
+            ?>
+            <ul id="tag-list-widget">
+            <?php
+            $tags = get_terms('post_tag', array(
+                'fields' => 'all', 
+                'orderby' => 'count',
+                'order' => 'DESC',
+                'number' => $instance['num']  ));
+            foreach ($tags as $tag) {
+                echo '<li><a href="/tag/'.$tag->slug.'">'.$tag->name.'<span class="count">'.$tag->count.'</span></a></li>';
+            }
+            
+            ?>
+            </ul>
+           <?php
+            echo $after_widget;
+    }
+    
+    function update( $new_instance, $old_instance ) {
+            $instance = $old_instance;
+            $instance['title'] = strip_tags($new_instance['title']);
+            $instance['num'] = (int)$new_instance['num'];
+            return $instance;
+    }
 
-	function form( $instance ) {
-			//Defaults
-				$instance = wp_parse_args( (array) $instance, array( 
-						'title' => 'Top Tags',
-						'num' => 45,
-							));	
-	?>  
+    function form( $instance ) {
+            //Defaults
+                $instance = wp_parse_args( (array) $instance, array( 
+                        'title' => 'Top Tags',
+                        'num' => 45,
+                            )); 
+    ?>  
         <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
         <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" /></p>
         
         <p><label for="<?php echo $this->get_field_id('num'); ?>"><?php _e('Number of links to show:'); ?></label> 
         <input class="widefat" id="<?php echo $this->get_field_id('num'); ?>" name="<?php echo $this->get_field_name('num'); ?>" type="text" value="<?php echo $instance['num']; ?>" /></p>
-		<?php
-	}
+        <?php
+    }
 }
 ?>
--- loop.php
+++ loop.php
@@ -18,20 +18,20 @@
  * @since Twenty Ten 1.0
  */?>
 
-<?php if ($_GET['trashed']) { ?>
-	<p class="notice">
-		<?php $trashedpost = get_post($_GET['ids']); printf(__('<em>%s</em> has been moved to the <a href="%s">trash.'), $trashedpost->post_title, get_option('site_url').'/wp-admin/edit.php?post_status=trash&post_type=post'); ?> 
-	</p>
+<?php if (array_key_exists('trashed', $_GET) && $_GET['trashed']) { ?>
+    <p class="notice">
+        <?php $trashedpost = get_post($_GET['ids']); printf(__('<em>%s</em> has been moved to the <a href="%s">trash.'), $trashedpost->post_title, get_option('site_url').'/wp-admin/edit.php?post_status=trash&post_type=post'); ?> 
+    </p>
 <?php } ?>
 
 <div id="tag-trail">
-	<?php if (!is_tag()) { ?>
-		<h1 class="page-title"><?php _e("tag filter:"); ?></h1>
-	<?php } ?>
-	
-	<form id="tag-filter" action="<?php echo $action; ?>" method=="GET">
-		<input type="text" name="tag" id="tag" />
-	</form>
+    <?php if (!is_tag()) { ?>
+        <h1 class="page-title"><?php _e("tag filter:"); ?></h1>
+    <?php } ?>
+    
+    <form id="tag-filter" action="<?php echo $action; ?>" method=="GET">
+        <input type="text" name="tag" id="tag" />
+    </form>
 </div><!-- # tag-trail -->
 
 <?php if (current_user_can('edit_posts')) { ?>
@@ -44,40 +44,40 @@
 <?php 
 /*
  if ( $wp_query->max_num_pages > 1 ) : ?>
-	<div id="nav-above" class="navigation">
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
-	</div><!-- #nav-above -->
+    <div id="nav-above" class="navigation">
+        <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
+        <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+    </div><!-- #nav-above -->
 <?php endif;
 /**/
  ?>
 
 <?php /* If there are no posts to display, such as an empty archive page */ ?>
 <?php if ( ! have_posts() ) : ?>
-	<div id="post-0" class="post error404 not-found">
-		<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
-		<div class="entry-content">
-			<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyten' ); ?></p>
-			<?php get_search_form(); ?>
-		</div><!-- .entry-content -->
-	</div><!-- #post-0 -->
+    <div id="post-0" class="post error404 not-found">
+        <h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
+        <div class="entry-content">
+            <p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyten' ); ?></p>
+            <?php get_search_form(); ?>
+        </div><!-- .entry-content -->
+    </div><!-- #post-0 -->
 <?php endif; ?>
 
 <?php
-	/* Start the Loop.
-	 *
-	 * In Twenty Ten we use the same loop in multiple contexts.
-	 * It is broken into three main parts: when we're displaying
-	 * posts that are in the gallery category, when we're displaying
-	 * posts in the asides category, and finally all other posts.
-	 *
-	 * Additionally, we sometimes check for whether we are on an
-	 * archive page, a search page, etc., allowing for small differences
-	 * in the loop on each template without actually duplicating
-	 * the rest of the loop that is shared.
-	 *
-	 * Without further ado, the loop:
-	 */
+    /* Start the Loop.
+     *
+     * In Twenty Ten we use the same loop in multiple contexts.
+     * It is broken into three main parts: when we're displaying
+     * posts that are in the gallery category, when we're displaying
+     * posts in the asides category, and finally all other posts.
+     *
+     * Additionally, we sometimes check for whether we are on an
+     * archive page, a search page, etc., allowing for small differences
+     * in the loop on each template without actually duplicating
+     * the rest of the loop that is shared.
+     *
+     * Without further ado, the loop:
+     */
 ?>
 <?php while ( have_posts() ) : the_post(); global $post; ?>
 
@@ -86,27 +86,27 @@
 <?php $first = FALSE; ?>
 <?php the_date('j M y', '<div class="day"><h2 class="date">', '</h2>', true); ?>
 
-		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<div class="entry-content">
-			<?php if(get_post_type($post) == 'bookmark'){ ?>
-				<a href="<?php echo get_post_meta(get_the_ID(), 'link_url', true) ?>" class="title"><?php the_title(); ?></a>
-				<?php if($desc=get_post_meta(get_the_ID(), 'link_desc', true)) { echo '<div class="description">' . $desc . '</div>'; } ?>
-			<?php }else{ ?>
-				<a href="<?php the_permalink(); ?>" class="title"><?php the_title(); ?></a>
-				<?php the_content(); ?>
-			<?php } ?>
-			</div><!-- .entry-content -->
+            <div class="entry-content">
+            <?php if(get_post_type($post) == 'bookmark'){ ?>
+                <a href="<?php echo get_post_meta(get_the_ID(), 'link_url', true) ?>" class="title"><?php the_title(); ?></a>
+                <?php if($desc=get_post_meta(get_the_ID(), 'link_desc', true)) { echo '<div class="description">' . $desc . '</div>'; } ?>
+            <?php }else{ ?>
+                <a href="<?php the_permalink(); ?>" class="title"><?php the_title(); ?></a>
+                <?php the_content(); ?>
+            <?php } ?>
+            </div><!-- .entry-content -->
 
-			<div class="entry-tags">
-				<?php the_tags('<span class="a-tag">', '', '</span>'); ?>
-			</div><!-- .entry-tags -->
+            <div class="entry-tags">
+                <?php the_tags('<span class="a-tag">', '', '</span>'); ?>
+            </div><!-- .entry-tags -->
 
-			<div class="entry-utility">
-				<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
-				<?php if (current_user_can('edit_posts')) { ?> <span class="meta-sep">|</span> <a href="<?php echo get_delete_post_link( $post->ID, '', false ) ?>">Delete</a> <?php } ?>
-			</div><!-- .entry-utility -->
-		</div><!-- #post-## -->
+            <div class="entry-utility">
+                <?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+                <?php if (current_user_can('edit_posts')) { ?> <span class="meta-sep">|</span> <a href="<?php echo get_delete_post_link( $post->ID, '', false ) ?>">Delete</a> <?php } ?>
+            </div><!-- .entry-utility -->
+        </div><!-- #post-## -->
 
 <?php endwhile; // End the loop. Whew. ?>
 </div><!-- day -->
@@ -115,10 +115,10 @@
 <?php
 /*
  if (  $wp_query->max_num_pages > 1 ) : ?>
-				<div id="nav-below" class="navigation">
-					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
-					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
-				</div><!-- #nav-below -->
+                <div id="nav-below" class="navigation">
+                    <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
+                    <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+                </div><!-- #nav-below -->
 <?php endif;
 /**/
  ?>
